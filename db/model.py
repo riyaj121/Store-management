2119fe170b16e31a4de02234d13c9e66d11b34fc
riyaj <68616044+riyaj121@users.noreply.github.com>
2020-09-06 20:12:37 +0400

    Update model.py

--- db/model.py
+++ db/model.py
@@ -5,6 +5,7 @@ from sqlalchemy import insert
 from sqlalchemy import select
 metadata=MetaData()
 
+# accessing the db
 try:
     engine=create_engine('mysql+pymysql://root:butterscotch@localhost/store',pool_recycle=3600)
 except:
@@ -15,6 +16,8 @@ conn=engine.connect()
 class Users:
 
     def __init__(self):
+
+        # defining the Table object to access the table users
         self.users=Table(
             'users',metadata,
             Column('id',Integer,primary_key=True,autoincrement=True),#default=10000),
@@ -26,12 +29,16 @@ class Users:
         )
     
     def add_user(self,u_Data):
+
+        # adding user to the user table
+        # args- u_Data: a dictionary having user details via form
         
         insertUser=self.users.insert().values(u_Data)
         res=conn.execute(insertUser)
 
 class Products:
 
+    # defining the Table object to access the table products 
     def __init__(self):
         self.products=Table
         (
@@ -42,16 +49,28 @@ class Products:
             Column('cost',Integer(),nullable=False)
         )
     
-        
+    # adding products to the product table
     def add_product(self,p_Data):
 
         insertProduct=self.products.insert().values(p_Data)
         res=conn.execute(insertProduct)
 
+    # displaying products according to filter - price, popularity , datetime
+    # arg- filter ( the condition by which items are sorted)
+    # filter by price, popularity, datetime
+    
     def show_product(self,filter):
-        
-        showPdt=select([self.products])
-        
+
+        if(filter=="price"):
+            col="cost"
+        elif(filter=="popularity"):
+            col=""
+        elif(filter=="datetime"):
+            col="reg_date"
+
+        # sorting products according to filter
+        showProduct=select([self.products]).group_by(col)
+        res=conn.execute(showProduct).fetchall()
 
 
 metadata.create_all(engine)
