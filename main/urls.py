441d27bc5f8d13a589a1a7dfcc5034bcfc1c14af
riyaj121 <68616044+riyaj121@users.noreply.github.com>
2020-09-13 20:04:51 +0400

    Revert "Updated files"
    This reverts commit 403ae399dad288c177f749408ba9bc58d1d15910.

--- main/urls.py
+++ main/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from app import view
+from main import view
 
 urlpatterns = [
     path('',view.wrongPath),
