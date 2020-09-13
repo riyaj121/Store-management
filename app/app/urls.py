403ae399dad288c177f749408ba9bc58d1d15910
riyaj121 <68616044+riyaj121@users.noreply.github.com>
2020-09-13 19:56:36 +0400

    Updated files

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from main import view
+from app import view
 
 urlpatterns = [
     path('',view.wrongPath),
